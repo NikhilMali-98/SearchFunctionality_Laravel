557eac7a3cf52a1b899668494c4ba9bd94e1f1d2
Mali Nikhil Pratap <134043327+NikhilMali-98@users.noreply.github.com>
2024-01-06 19:49:35 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,17 @@ use App\Http\Controllers\ProductController;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/products',[ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create',[ProductController::class, 'create'])->name('products.create');
 Route::post('/products/store',[ProductController::class, 'store'])->name('products.store');
 Route::get('/products/{id}',[ProductController::class, 'show'])->name('products.show');
+
 
 
 // for serach functionality 
